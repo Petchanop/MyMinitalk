768d78fa971a141abd03dda1a0fc740a34ea7fd3
Noppadon Piya-isarakul <npiya-is@z3t7c1.42bangkok.com>
2022-09-04 00:35:56 +0700

    nearly finish

--- minitalk.h
+++ minitalk.h
@@ -3,31 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: npiya-is <npiya-is@student.42.fr>          +#+  +:+       +#+        */
+/*   By: npiya-is <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 12:39:30 by npiya-is          #+#    #+#             */
-/*   Updated: 2022/06/29 14:37:02 by npiya-is         ###   ########.fr       */
+/*   Updated: 2022/09/04 00:31:55 by npiya-is         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define    MINITALK_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <limits.h>
-#include "ft_printf/libft/libft.h"
-#include "ft_printf/srcs/ft_printf.h"
-
-typedef struct s_data
-{
-    char            *buffer;
-    unsigned char   c;
-    int             index;
-    int             client_pid;
-}               t_data;
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <limits.h>
+# include "ft_printf/libft/libft.h"
+# include "ft_printf/srcs/ft_printf.h"
 
 #endif
-
--- server/minitalk_server.c
+++ server/minitalk_server.c
@@ -6,46 +6,17 @@
 /*   By: npiya-is <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 12:39:01 by npiya-is          #+#    #+#             */
-/*   Updated: 2022/09/03 21:36:25 by npiya-is         ###   ########.fr       */
+/*   Updated: 2022/09/04 00:34:35 by npiya-is         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-t_data	recieve;
-
-int	send_char_to_server(unsigned char c, int server_pid)
-{
-	unsigned char	bit;
-
-	bit = 128;
-	while (bit)
-	{
-		if (bit & c)
-		{
-			if (kill(server_pid, SIGUSR1) == -1)
-				return (0);
-		}
-		else
-		{
-			if (kill(server_pid, SIGUSR2) == -1)
-				return (0);
-		}
-		bit >>= 1;
-		usleep(100);
-	}
-	return (1);
-}
-
-static void	ft_sighandler(int signum, siginfo_t *info)
+static void	ft_sighandler(int signum, siginfo_t *info, void *context)
 {
 	static int				data_index = 8;
 	static unsigned char	bit = 0;
-	char					*str;
-	int						i;
 
-	i = 0;
-	str = "RECEIVE signal by pid :";
 	if (data_index > 0)
 	{
 		data_index--;
@@ -56,16 +27,12 @@ static void	ft_sighandler(int signum, siginfo_t *info)
 	}
 	if (data_index == 0)
 	{
-		if (bit == 0)// && !info->si_pid)
+		if (bit == 0 || !context)
 		{
-			write(1, "\n", 2);
 			usleep(1000);
-			while ((i < 5) & str[i])
-			{
-				send_char_to_server(str[i], info->si_pid);
-				i++;
-			}
-			send_char_to_server(0, info->si_pid);
+			kill(info->si_pid, SIGUSR1);
+			write(1, "\n", 2);
+			ft_printf("receive signal from : %d\n", info->si_pid);
 		}
 		write(1, &bit, 1);
 		data_index = 8;
@@ -75,9 +42,7 @@ static void	ft_sighandler(int signum, siginfo_t *info)
 
 static void	ft_response(int signum, siginfo_t *info, void *context)
 {
-	ft_sighandler(signum, info);
-	if (!signum || !context )
-		ft_printf("\ncomplete %d", info->si_pid);
+	ft_sighandler(signum, info, context);
 }
 
 void	ft_getpid(void)
@@ -87,14 +52,11 @@ void	ft_getpid(void)
 
 int	main(void)
 {
-	struct sigaction client_sig;
-   // client_sig.sa_handler = ft_sighandler;
-   client_sig.sa_flags = SA_SIGINFO;
-   client_sig.sa_sigaction = ft_response; 
-   //struct sigaction client_sig2;
+	struct sigaction	client_sig;
+
+	client_sig.sa_flags = SA_SIGINFO;
+	client_sig.sa_sigaction = &ft_response;
 	ft_getpid();
-	//signal(SIGUSR1, ft_sighandler);
-	//signal(SIGUSR2, ft_sighandler);
 	sigaction(SIGUSR1, &client_sig, NULL);
 	sigaction(SIGUSR2, &client_sig, NULL);
 	while (1)
